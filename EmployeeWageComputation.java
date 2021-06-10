4ccf92fbbc41f9c4c108baa496e414bd45559488
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-10 12:36:35 -0400

    add : EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,10 +1,59 @@
 package com;
 
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+
+/**
+ * company is class of public type
+ * @author rekha
+ *
+ */
+
+class CompanyInfo
+{
+    // VARIABLES
+    public final String company;
+    public final int WAGE_PER_HOUR;
+    public final int WORKING_DAYS_PER_MONTH;
+    public final int MAX_WORKING_HRS;
+    private int totalWage;
+    public ArrayList<Integer> dailyWage;
+
+    public CompanyInfo(String company, int wage_per_hr, int working_days_per_month, int max_working_hrs)
+    {
+        this.company = company;
+        this.WAGE_PER_HOUR = wage_per_hr;
+        this.WORKING_DAYS_PER_MONTH = working_days_per_month;
+        this.MAX_WORKING_HRS = max_working_hrs;
+    }
+
+    public void setTotalWage(int totalWage)
+    {
+        this.totalWage = totalWage;
+    }
+
+    public void setDailyWage(ArrayList<Integer> daily_wage)
+    {
+        this.dailyWage = daily_wage;
+    }
+
+    public ArrayList<Integer> getDailyWages()
+    {
+        return dailyWage;
+    }
+
+    public int getTotalWage()
+    {
+        return totalWage;
+    }
+
+    public String toString()
+    {
+        System.out.println("Company: "+company);
+        return "DailyWage: "+dailyWage+"\n"+"Total Wage: "+totalWage;
+    }
+}
 
 /**
  * EmployeeWageComputation is a class of public type
@@ -12,148 +61,111 @@ import java.util.Map;
  * Implementing IEmpWageBuilder
  */
 
+//MAIN CLASS
+
+
 public class EmployeeWageComputation implements IEmpWageBuilder
-{	
-	public static final int IS_PART_TIME = 1 ;
-	public static final int IS_FULL_TIME = 2;
-	private int numOfCompany = 0;
-	private LinkedList<CompanyEmpwage> companyEmpWageList;
-	private Map<String,CompanyEmpwage> companyToEmpWageMap;
-	
-/**
- * EmployeeWageComputation is a constructor of public class
- * Initializing an array of CompanyEmpWage  
- * @return 
- */
-	public EmployeeWageComputation()
-	{
-		companyEmpWageList = new LinkedList<>();
-		companyToEmpWageMap = new HashMap<>();
-	}
-
-	/**
-	 * computeEmpWage is a function which is use to calculate the employee wage .
-	 * function is void type which returns nothing .	
-	 */
-	
-	public void computeEmpWage() 
-	{
-		for (int i = 0; i < companyEmpWageList.size(); i++)
-		{
-			CompanyEmpwage companyEmpWage = companyEmpWageList.get(i);
-			companyEmpWage.setTotalEmpwage(this.computeEmpwage(companyEmpWage));
-			System.out.println(companyEmpWage);	
-		}
-	}
-	
-	/**
-	 *  using here the method overloading because using the same name of the method with different parameter. 
-	 *  Here we are calculating total working days and total employee hour.
-	 * @param companyEmpwage is a argument .
-	 * @return integer  value .
-	 */
-	
-	private int computeEmpwage(CompanyEmpwage companyEmpwage) 
-	{
-		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-		while (totalEmpHrs < companyEmpwage.MAX_HOURS_PER_MONTH && totalWorkingDays<companyEmpwage.NUM_OF_WORKING_DAYS) 
-		{
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) %3;
-			switch (empCheck) 
-			{
-				case IS_PART_TIME:
-					empHrs = 4;
-				break;
-				case IS_FULL_TIME:
-					empHrs = 8;
-				break;
-				default:
-					empHrs = 0;
-			}
-				totalEmpHrs += empHrs;
-				System.out.println("Total working Days : " +totalWorkingDays+   " Total Employee Hour : "+empHrs);
-		}
-		return totalEmpHrs * companyEmpwage.EMP_RATE_PER_HOUR;	
-	}
-	
-	/**
-	 * This is a main class where we are calling the function . 
-	 * passing the data of companies to calculate the employee wage.
-	 * @param args
-	 */
-	
-	public static void main(String[] args) 
-	{
-		IEmpWageBuilder empWageBuilder = new EmployeeWageComputation();
-		empWageBuilder.addCompanyEmpWage("Dmart",20,2,24);
-		empWageBuilder.computeEmpWage();
-		System.out.println("Total Wage for Dmart Company : " + empWageBuilder.getTotalWage1("Dmart"));
-	}
-	
-	/**
-	 * addCompanyEmpWage is a a function of private type
-	 * @param company , empRatePerHour , numorworkingDays , maxHoursPerMonth as a argument
-	 * created a array of companyEmpWage which stores the number of company 
-	 * cling the constructor of company employee wage 
-	 */
-		
-	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) 
-	{
-		
-		CompanyEmpwage companyEmpWage= new CompanyEmpwage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
-		companyEmpWageList.add(companyEmpWage);
-		companyToEmpWageMap.put(company,companyEmpWage);
-	}
-
-	
-	public int getTotalWage1(String company)
-	{
-		
-		return companyToEmpWageMap.get(company).totalEmpwage;
-	}
-	
-	
-	
-}
-
-/**
- * created a another class of Company Employee Wage
- * taken some variable as a final which will never change.
- *
- */
-
-class CompanyEmpwage
+    // CONSTANTS
 {
-	public final String COMPANNY;
-	public final int EMP_RATE_PER_HOUR;
-	public final int NUM_OF_WORKING_DAYS;
-	public final int MAX_HOURS_PER_MONTH;
-	public int totalEmpwage;
-/**
- * Created a parameterize constructor of Company employee wage 
- * @param company , empRatePerHour , numOfWorkingDays , maxHoursPerMonth as argument
- */
-	public CompanyEmpwage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth)
-	{
-		this.COMPANNY = company;
-		this.EMP_RATE_PER_HOUR = empRatePerHour;
-		this.NUM_OF_WORKING_DAYS = numOfWorkingDays;
-		this.MAX_HOURS_PER_MONTH = maxHoursPerMonth;
-	}
-	
-	/**
-	 * Created a seater of total employee wage use to set the value of totalEmpwage
-	 * @param totalEmpwage as a argument.
-	 */
-	
-	public void setTotalEmpwage(int totalEmpwage) 
-	{
-		this.totalEmpwage = totalEmpwage;
-	}	
-	public String toString()
-	{
-		return "Total Emp Wage for Company:" +COMPANNY+ " is :" + totalEmpwage;
-	}
+    public final int IS_PRESENT = 1;
+    public final int IS_FULL_TIME = 1;
+
+    ArrayList<CompanyInfo> totalWageofDiffEmp = new ArrayList<CompanyInfo>();;
+
+    //Object for Company Info method
+    CompanyInfo wcc;
+
+    public void company(String company, int wage_per_hr, int working_days_per_month, int max_working_hrs)
+    {
+        //Adding details to the array list (array list of type 'CompanyInfo')
+        wcc = new CompanyInfo(company, wage_per_hr, working_days_per_month, max_working_hrs);
+
+        totalWageofDiffEmp.add(wcc);
+
+        CompanyInfo cI;
+
+        ArrayList<Integer> al2;
+
+        
+        
+        //calling calculator method for every company
+        for(int i=0;i<totalWageofDiffEmp.size();i++)
+        {
+            cI = totalWageofDiffEmp.get(0);
+
+            al2 = wageCalculator(cI);
+            int totalWage = al2.get(al2.size()-1);
+            wcc.setDailyWage(al2);
+            wcc.setTotalWage(totalWage);
+            //map.put(company, totalWage);//<-----------NULL POINTER EXCEPTION*
+
+            System.out.println(cI);
+        }
+    }
+
+    // WAGE CALCULATOR
+    public ArrayList<Integer> wageCalculator(CompanyInfo cI)
+    {
+        // VARIABLES
+        int checkPresence;
+        int checkEmpType;
+        int empDailyWage = 0;
+        int dayOfMonth = 0;
+        int totalWage;
+        int workingHrs = 0;
+        int totalWorkedHrs = 0;
+
+        //array-list
+        ArrayList<Integer> al1 = new ArrayList<Integer>();
+
+        // Wage calculation
+        while (dayOfMonth <= cI.WORKING_DAYS_PER_MONTH && totalWorkedHrs <= cI.MAX_WORKING_HRS)
+        {
+            checkPresence = (int) (Math.random() * 10) % 2; // Presence check
+            checkEmpType = (int) (Math.random() * 10) % 2; // Work Type check
+            dayOfMonth++;
+            switch (checkPresence)
+            {
+                case IS_PRESENT:
+                    switch (checkEmpType)
+                    {
+                        case IS_FULL_TIME:
+                            workingHrs = 8;
+                            totalWorkedHrs += workingHrs;
+                            break;
+                        default:
+                            workingHrs = 4;
+                            totalWorkedHrs += workingHrs;
+                    }
+                    break;
+                default:
+                    totalWorkedHrs += 0;
+            }
+
+            empDailyWage = cI.WAGE_PER_HOUR * workingHrs;
+            al1.add(empDailyWage);
+
+            totalWage = (cI.WAGE_PER_HOUR * totalWorkedHrs);
+
+            al1.add(totalWage);
+
+        }
+        return al1;
+    }
+    
+    /**
+     * Main method
+     * @author rekha
+     *EmployeeWageComputation is a class here created company1 and company2 objects  
+     */
+
+    public static void main(String[] args)
+    {
+        EmployeeWageComputation company1 = new EmployeeWageComputation();
+        EmployeeWageComputation company2 = new EmployeeWageComputation();
+
+        company1.company("company1",20, 20, 100);
+        company2.company("company2",25, 15, 150);
+        
+    }
 }
