f9e16f8839e39b55195ebefaf0dc88587f8a9f82
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-09 22:10:30 -0400

    add : EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,87 +1,128 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
- * EmpWageMultipleCompanies is a class of public type
- * @author rekha
- *Initializing two static variable
+ * EmployeeWageComputation  is a class of public type
+ * Initializing two static variable
  */
+public class EmployeeWageComputation 
+{	
+	public static final int IS_PART_TIME = 1 ;
+	public static final int IS_FULL_TIME = 2;
 
- class EmployeeWageComputation 
-{		
-	public final static int FULL_DAY = 1;
-	public final static int HALF_DAY = 2;
-	
-	private final String COMPANY_NAME ;
-	private final int WAGE_PER_HOUR ;
-	private int workDays ;
-	private final int MAX_HOURS;
-	public int totalSallary;
-	
-	/**
-	 * Created a parameterize constructor of Company employee wage 
-	 * @param company , empRatePerHour , numOfWorkingDays , maxHoursPerMonth as argument
-	 */
-	
-	public EmployeeWageComputation(String companyName , int WagePerHour , int workDays , int MaxHours)
+	private int numOfCompany = 0;
+	private CompanyEmpwage[] companyEmpwageArray;
+/**
+ * EmployeeWageComputation  is a constructor of public class
+ * Initializing an array of CompanyEmpWage  
+ */
+	public EmployeeWageComputation()
 	{
-		this.COMPANY_NAME = companyName;
-		this.WAGE_PER_HOUR = WagePerHour;
-		this.workDays = workDays;
-		this.MAX_HOURS = MaxHours;
+		companyEmpwageArray = new CompanyEmpwage[5];
 	}
-	
+/**
+ * addCompanyEmpWage is a a function of private type
+ * @param company , empRatePerHour , numorworkingDays , maxHoursPerMonth as a argument
+ * created a array of companyEmpWage which stores the number of company 
+ * calling the constructor of company employewage 
+ */
+	private void addCompanyEmpwage(String company, int empRatePerHour,int numorworkingDays, int maxHoursPerMonth) 
+	{
+		companyEmpwageArray[numOfCompany] = new CompanyEmpwage(company,empRatePerHour,numorworkingDays,maxHoursPerMonth);
+		numOfCompany++;
+	}
 	/**
-	 * creating calculateEmpWage method to calculate the employee wage
-	 * Here we are Calculating employee wage
-	 * using random function to generate 0 to2 random numbers
-	 * Here we are calculating total working days, total employee hour and total salary of employee
-	 *Returning total Salary of employee
+	 * computeEmpWage is a function which is use to calculate the employee wage .
+	 * function is void type which returns nothing .	
 	 */
-		
-		public double calculateEmpWage() 
+	private void computeEmpWage() 
+	{
+		for (int i = 0; i < numOfCompany; i++)
 		{
-			int workHours = 0 ,empHours = 0 ;
-			int TotalWorkDays = 0;
-			while(workHours <= MAX_HOURS && TotalWorkDays <= workDays)
+			companyEmpwageArray[i].setTotalEmpwage(this.computeEmpwage(companyEmpwageArray[i]));
+			System.out.println(companyEmpwageArray[i]);	
+		}
+	}
+	/**
+	 *  using here the method overloading because using the same name of the method with different parameter. 
+	 *  Here we are calculating total working days and total employee hour.
+	 * @param companyEmpwage is a argument .
+	 * @return integer  value .
+	 */
+	private int computeEmpwage(CompanyEmpwage companyEmpwage) 
+	{
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+		while (totalEmpHrs < companyEmpwage.maxHoursPerMonth && totalWorkingDays<companyEmpwage.numOfWorkingDays) 
+		{
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) %3;
+			switch (empCheck) 
 			{
-				workDays++ ;	
-				int attendance = (int) Math.floor(Math.random() * 10) % 3; 
-				switch (attendance)
-				{
-					case FULL_DAY: 
-						empHours = 8;
-					break;
-					case HALF_DAY: 
-						empHours = 4;
-					break;
-					default: 
-						empHours = 0;
-				}
-				workHours += empHours;
-			
+				case IS_PART_TIME:
+					empHrs = 4;
+				break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+				break;
+				default:
+					empHrs = 0;
 			}
-			System.out.println("Total working days : " + workDays +  "  Total working hours : " + workHours );
-			int totalSalary = ( WAGE_PER_HOUR * workHours);
-			return totalSalary;
+				totalEmpHrs += empHrs;
+				System.out.println("Total working Days : " +totalWorkingDays+   " Total Employee Hour : "+empHrs );
 		}
-		
-		/**
-		 * Printing Welcome statement
-		 * passing the days of 2 companies by providing company total working days, total working hours in a day , and salary per hour 
-		 * to calculate the employee total wage.
-		 *
-		 */
-		
-		
-		public static void main(String[] args)
-		{
-			System.out.println("Welcome in employee wage");
-			
-			EmployeeWageComputation Honda = new EmployeeWageComputation("Honda", 50, 7, 10);
-			EmployeeWageComputation Relience = new EmployeeWageComputation("Relience", 50, 6, 10);
-			double salaryHonda = Honda.calculateEmpWage();
-			System.out.println("Employe Wage of honda is : " +salaryHonda);
-			double salaryRelience =Relience.calculateEmpWage();
-			System.out.println("Employe Wage of relience is : " +salaryRelience);
-		}
+		return totalEmpHrs * companyEmpwage.empRatePerHour;	
+	}
+	/**
+	 * This is a main class where we are calling the function . 
+	 * EmployeeWageComputation is a class in that crating new object empWageBuilder
+	 * passing the daya of 2 companies to calculate the employee wage.
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		EmployeeWageComputation empWageBuilder = new EmployeeWageComputation();
+
+		empWageBuilder.addCompanyEmpwage("DMart", 28, 10, 10); 
+		empWageBuilder.addCompanyEmpwage("Reliance", 18, 8, 20);
+
+		empWageBuilder.computeEmpWage();
+	}
+}
+/**
+ * created a another class of Company Employee Wage
+ * taken some variable as a final which will never change.
+ *
+ */
+class CompanyEmpwage
+{
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpwage;
+/**
+ * Created a parameterize constructor of Company employee wage 
+ * @param company , empRatePerHour , numOfWorkingDays , maxHoursPerMonth as argument
+ */
+	public CompanyEmpwage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth)
+	{
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+	/**
+	 * Created a seater of total employee wage use to set the value of totalEmpwage
+	 * @param totalEmpwage as a argument.
+	 */
+	public void setTotalEmpwage(int totalEmpwage) 
+	{
+		this.totalEmpwage = totalEmpwage;
+	}	
+	public String toString()
+	{
+		return "Total Emp Wage for Company:" +company+ " is :" + totalEmpwage;
+	}
 }
