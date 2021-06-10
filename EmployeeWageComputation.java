733d76d8aa3d1534f2400a378cbd0cce98a62c70
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-09 21:24:55 -0400

    add : EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,37 +2,41 @@ package com;
 
 public class EmployeeWageComputation 
 {
-	      
-//Constants variables
 		  public static final int IS_ABSENT = 0;
 		  public static final int IS_PRESENT = 1;
+		  public static final int EMP_RATE_PER_HOUR = 20;
+		  public static final int FULL_DAY_HOUR = 8;
+		  public static int salary = 0;
 		  
-		  /**
-		 * generating random number 0 or 1
-		 * checking and displaying if employee is present or absent
-		 */
-		public static void present() 
+	        /**
+	        * generating random number 0 or 1
+	        * checking and displaying if employee is present or absent
+	        * Calculating employee day salary
+	        */  
+		  
+		  public static int computeEmpWage() 
 		  {
-			  int emp_Check = (int) Math.floor(Math.random() * 10 ) % 2;
+			  int emp_Check = (int)  Math.floor(Math.random() * 10) % 2;
+			  
 			  switch(emp_Check)
 			  {
+			     case IS_PRESENT:System.out.println("Employee is present and his salary is:");
+			                   salary = EMP_RATE_PER_HOUR * FULL_DAY_HOUR;
+			                   break;
 			     case IS_ABSENT:System.out.println("Employee is absent");
-			                  break;
-			     case IS_PRESENT:System.out.println("Employee is present");
-			                  break;
-		   }
-	
-		}
+			          	   break;
+			  }
+			  return salary;
+		 }
 		  
-		/**
-		 * Calling function present
-		 * @params args
-		 */
-		
-		public static void main(String[] args) 
-		
-		{
-				System.out.println("Welcome to Employee Wage Computation Problems");	
-		present();
-		}
-	}
+		  /**
+			 * Calling and printing function computeEmpWage
+			 * 
+			 */
+		  
+		 public static void main(String[] args) 
+		 {
+			 System.out.println(computeEmpWage());
+		 }
+
+}
