0005dde85d2f5e41d08f0b3131b9063cbbd2be32
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-09 21:13:50 -0400

    add : EmployeeWageComputation.java

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com;
+
+public class EmployeeWageComputation 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Employee Wage Computation Problems");
+	}
+
+}
